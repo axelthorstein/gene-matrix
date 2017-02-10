f9f939967e5f65fc0980ea5a4f7e7ac0aa5c7310
Axel Thor <axelthorstein@gmail.com>
2017-02-09 20:49:18 -0600

    added error catching for commandline arguments

--- process_gene_sequence.py
+++ process_gene_sequence.py
@@ -3,6 +3,11 @@ from modules import format, combine, gene_matrix, spellcheck
 from subprocess import call
 from multiprocessing import Pool
 
+def check_file_ending(out_file):
+	if "." in out_file:
+		return out_file[:out_file.index(".")]
+	return out_file
+
 def align(gene_type):
 	in_file = "gene_files/unaligned_gene_files/" + gene_type
 	out_file = "gene_files/aligned_gene_files/" + gene_type
@@ -18,6 +23,8 @@ if __name__ == '__main__':
 	parser.add_argument('--startstep', choices=["combine", "spellcheck", "align", "format", "matrix"], 
 		help='Select which step you would like the process to begin with, '
                    +'which assumes the directory that you are passing in is ready to be processed at that step. (default=combine)')
+	parser.add_argument('--endstep', choices=["combine", "spellcheck", "align", "format", "matrix"], 
+		help='Select which step you would like the process to end with. (default=matrix)')
 	parser.add_argument('--type', choices=["fasta", "nexus", "phylip"], 
 		help='Sepcify output file type [fasta, nexus, phylip]')
 	parser.add_argument('--o', 
@@ -30,12 +37,16 @@ if __name__ == '__main__':
 	# Set default parameters
 	if args.startstep is None:
 		args.startstep = "combine"
+	if args.endstep is None:
+		args.endstep = "matrix"
 	if args.pool is None:
 		args.pool = 1
 	if args.o is None:
 		args.o = 'output'
 	if args.type is None:
 		args.type = 'fasta'
+
+	args.o = check_file_ending(args.o)
 
 	# Create all the necessary directories.
 	parent_dir = os.getcwd() + "/"
@@ -60,14 +71,16 @@ if __name__ == '__main__':
 		sys.stdout.write("Combining single gene Sequences.\n")
 		filenames = combine.combine_gene_sequences(filenames, parent_dir + sys.argv[1], unaligned)
 		sys.stdout.write("Combination finished.\n")
-		args.startstep = "spellcheck"
+		if args.endstep != "combine":
+			args.startstep = "spellcheck"
 
 	if args.startstep == "spellcheck":
 		# Check the spelling of the collected species names.
 		sys.stdout.write("Checking spelling.\n")
 		spellcheck.misspelled_check(filenames, unaligned)
 		sys.stdout.write("Spellcheck finished.\n")
-		args.startstep = "align"
+		if args.endstep != "spellcheck":
+			args.startstep = "align"
 
 	if args.startstep == "align":
 		# Align all of the sequences using the Muscle algorithm in a process pool. 
@@ -79,14 +92,16 @@ if __name__ == '__main__':
 			pool = Pool(processes = 1)
 		pool.map(align, filenames)
 		sys.stdout.write("\nSequence alignment finished. \n")
-		args.startstep = "format"
+		if args.endstep != "align":
+			args.startstep = "format"
 
 	if args.startstep == "format":
 		# Format all of the sequences. 
 		sys.stdout.write("Formatting files.\n")
 		format.format(filenames, aligned, formatted)
 		sys.stdout.write("Formatting successful.\n")
-		args.startstep = "matrix"
+		if args.endstep != "format":
+			args.startstep = "matrix"
 
 	# Add exta species. 
 
